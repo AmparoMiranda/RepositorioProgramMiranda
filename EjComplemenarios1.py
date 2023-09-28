c6e63d131089c74272b09540fb7388ba83c60c86
AmparoMiranda <145479244+AmparoMiranda@users.noreply.github.com>
2023-09-28 11:46:25 -0300

    Add files via upload
    TP Ejercicios Complementarios semana 2

---
+++ EjComplemenarios1.py
@@ -0,0 +1,86 @@
+#Ejercicio 1
+numero1 = 23
+
+#Ejercicio 2
+numero2 = 2.3
+
+#Ejercicio 3
+suma = numero1 + numero2
+
+#Ejercicio 4
+resta = numero1 - numero2
+print(f'Resta: {numero1} - {numero2} = {resta}')
+multiplicacion = numero1 * numero2
+print(f'Multiplicacion: {numero1} * {numero2} = {multiplicacion}')
+division = numero1 / numero2
+print(f'Division: {numero1} / {numero2} = {division}')
+print(f' ')
+#Ejercicio 5
+nombre = "Amparo"
+
+#Ejercicio 6
+precio = 10.99
+
+#Ejercicio 7
+descuento = 0.25
+
+#Ejercicio 8
+precio_final = precio * descuento
+print(f'Precio sin descuento: {precio}')
+print(f'Precio con descuento: {precio_final}')
+print(f' ')
+
+#Ejercicio9
+cadena = "Feliz primavera"
+print(f'Cadena: {cadena}')
+print(f' ')
+
+#Ejercicio 10
+longitud = len(cadena)
+print(f'Longitud de la cadena: {longitud}')
+print(f' ')
+
+#Ejercicio 11
+precio = int(3.4)
+print(f'Precio: {precio}')
+print(f' ')
+
+#Ejercicio 12
+nombre = "Amparo"
+print(f'Nombre: {nombre}')
+apellido = "Miranda"
+print(f'Apellido: {apellido}')
+nombre_completo = nombre + apellido
+print(f'Nombre completo: {nombre_completo}')
+print(f' ')
+
+#Ejercicio 13
+edad = 19
+print(f'Edad: {edad}')
+edad += 5
+print(f'Edad + 5: {edad}')
+edad -= 10
+print(f'Edad - 10: {edad}')
+print(f' ')
+
+#Ejericio 14
+altura = 1.57
+print(f'Altura: {altura}')
+altura *= 4
+print(f'Altura x 4: {altura}')
+altura /= 3
+print(f'Altura dividio 3: {altura}')
+print(f' ')
+
+#Ejercicio 15
+nombre_may = "AMPARO"
+print(f'Nombre en mayusuclas: {nombre_may}')
+nombre_min = nombre_may.lower()
+print(f'Nombre en minusculas: {nombre_min}')
+print(f' ')
+
+#Ejercicio 16
+nombre_final = nombre_may
+nombre_final = nombre_final[0] + nombre_final[1:].lower()
+print(f'Nombre con la primera letra en mayuscula: {nombre_final}')
+
